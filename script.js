3d11114801eb71d5769d2b804bc9052bf5d00fe4
AMOUSSOU Christian <christian.amoussou@epitech.eu>
2024-03-08 22:18:05 +0100

    fix: call of img

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-axios.get("https://api.kedufront.juniortaker.com/item/")
+const container = document.getElementById('container')
+
+async function getArticle() {
+    let resultat = await axios.get("https://api.kedufront.juniortaker.com/item/")
+    return resultat;
+}
+
+getArticle()
 .then(function(response) {
-    console.log(response.data[0]);
+    console.log(response)
+    response.data.forEach ((article) => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML = `
+        <img src="https://api.kedufront.juniortaker.com/item/picture/${article.image}" alt="image_peluche">
+        <h3>${article.name}</h3>
+        <p>${article.price} $</p>
+        `
+        container.appendChild(card)
+      })
+    // console.log(response.data[0]);
   console.log(response);
 })
 .catch(function(error) {
   console.log(error);
 });
+
+// img src="article.id
