ac1290135f22a9bd17978e630c23e98fcaebf784
AMOUSSOU Christian <christian.amoussou@epitech.eu>
2024-03-10 14:23:54 +0100

    add: responsive ipad

--- script.js
+++ script.js
@@ -25,4 +25,17 @@ getArticle()
   console.log(error);
 });
 
-// img src="article.id
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
